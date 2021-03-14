fd7bd278d0ac92a657e068a95e01cb418aa514bc
Dwij Mehta <dwij.mehta@gmail.com>
2021-03-14 16:09:03 +0530

    Updated team and user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,29 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const validator = require("validator");
+
 let User = new Schema({
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true,
     },
-    googleID: {
-        type: String
+    emailId: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Email is invalid");
+            }
+        },
     },
+
+    teamID: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: null,
+    }
 });
 module.exports = mongoose.model('User', User);
