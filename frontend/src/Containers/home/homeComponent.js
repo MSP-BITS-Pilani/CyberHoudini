45f54ce8620daf79d4eb26f4cb7394a4067fff37
madhavgupta211 <56752137+madhavgupta211@users.noreply.github.com>
2021-03-15 22:01:03 +0530

    something

--- frontend/src/Containers/home/homeComponent.js
+++ frontend/src/Containers/home/homeComponent.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import './home.css';
+import { Button } from 'shards-react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return(
-      <h1>Hello</h1>
+      <div className = "container">
+        <br/>
+        <h4>Register for the online treasure hunt of the millenia by MLSA this apogee 21.</h4>
+        <br/>
+        <Button theme = "warning">
+          <Link to = "/register">
+            Register now
+          </Link>
+        </Button>
+      </div>
     );
   }
 }
