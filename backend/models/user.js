6581bb86aa2e8223a92d2790755d1fa99253d9ca
Dwij Mehta <55494681+dwij2212@users.noreply.github.com>
2021-03-16 00:36:36 +0530

    Dir struct (#6)
    * Added middleware
    
    * Improved directory structure

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const validator = require("validator");
+const jwt = require('jsonwebtoken');
 
-let User = new Schema({
+let userSchema = new Schema({
     name: {
         type: String,
         required: true,
         trim: true,
     },
-    emailId: {
+    email: {
         type: String,
         required: true,
         trim: true,
@@ -24,6 +25,50 @@ let User = new Schema({
     teamID: {
         type: mongoose.Schema.Types.ObjectId,
         default: null,
+        ref: 'Team'
+    },
+
+    picture: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+
+    tokens: [{
+        token: {
+            type: String
+        }
+    }],
+
+    oauth_tokens: {
+        access_token: {
+            type: String
+        },
+        refresh_token: {
+            type: String
+        }
     }
 });
-module.exports = mongoose.model('User', User);
+
+// Overriding the default method to return user so get users doesnt return tokens
+userSchema.methods.toJSON = function () {
+    const user = this.toObject();
+
+    delete user.tokens;
+    delete user.oauth_tokens;
+
+    return user;
+};
+
+// generate authentication token for user using jwt
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.SECRET_KEY);
+    user.tokens = user.tokens.concat({ token: token });
+    await user.save();
+
+    return token;
+};
+
+module.exports = mongoose.model('User', userSchema);
