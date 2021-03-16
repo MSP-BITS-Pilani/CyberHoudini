2afe07b8a3919083fbe0e2175be77648d82cd0e2
Dwij Mehta <55494681+dwij2212@users.noreply.github.com>
2021-03-17 00:52:42 +0530

    Started working on user api (#11)
    * started working on user api
    
    * GET /user API implemented

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,39 +1,15 @@
 const express = require("express")
 const userRouter = express.Router();
 const User = require("../models/user")
+const auth = require("../middleware/auth")
 
-userRouter.get("/", (req, res) => {
-    User.find((err, users) => {
-        if (err) {
-            console.error(err);
-        } else {
-            if (users.length == 0) {
-                var testUsers = [
-                    { name: 'cor', desc: 'person who does the thing' },
-                    { name: 'jynnie', desc: 'person who does the thing' },
-                    { name: 'mntan', desc: 'person who does the thing' }
-                ];
-
-                User.collection.insert(testUsers, (err, users) => { if (err) console.log(err); })
-            }
-            res.send(users)
-        }
-    })
-})
-userRouter.get("/UserDetails",(req,res)=>{
-    if(!req.query.email){
-        return res.send({
-            error:"Please Enter email id"
-        })
-    }
-    const email=req.query.email
-    //const email="xxx123@gmail.com"
-    const user=User.findOne({email: email})
-    if(!user){
+userRouter.get("/", auth, (req, res) => {
+    const user = req.user
+    if (!user) {
         console.log('Email address not registered')
     }
     else
-    res.send(user)
+        res.send(user)
 })
 
 
