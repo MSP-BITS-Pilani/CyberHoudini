c88178d3328a6b3a1229a6d4666049ad025cd4b7
Dwij Mehta <dwij.mehta@gmail.com>
2021-03-16 23:24:54 +0530

    GET /user API implemented

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,7 @@ const userRouter = express.Router();
 const User = require("../models/user")
 const auth = require("../middleware/auth")
 
-userRouter.get("/user", auth, (req, res) => {
+userRouter.get("/", auth, (req, res) => {
     const user = req.user
     if (!user) {
         console.log('Email address not registered')
