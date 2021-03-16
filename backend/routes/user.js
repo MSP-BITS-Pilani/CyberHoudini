1f08814aa002154667b9e511420bb68f06417108
Dwij Mehta <55494681+dwij2212@users.noreply.github.com>
2021-03-17 01:09:05 +0530

    User API returns team data (#13)
    * started working on user api
    
    * GET /user API implemented
    
    * GET /user returns team details
    
    * Completed GET /user API

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,16 +1,22 @@
 const express = require("express")
 const userRouter = express.Router();
-const User = require("../models/user")
+const Team = require("../models/team")
 const auth = require("../middleware/auth")
 
-userRouter.get("/", auth, (req, res) => {
+userRouter.get("/", auth, async (req, res) => {
     const user = req.user
+
     if (!user) {
         console.log('Email address not registered')
+        res.sendStatus(404)
     }
-    else
-        res.send(user)
-})
+    else {
+        const team = await Team.findById(user.teamID);
+        res.send({ user, team })
+    }
+}
+
+)
 
 
 
