e10dd42f4cc1574616a76658ffec5db30317a485
madhavgupta211 <56752137+madhavgupta211@users.noreply.github.com>
2021-03-16 10:17:00 +0530

    register component being created

--- frontend/src/Containers/register/registerComponent.js
+++ frontend/src/Containers/register/registerComponent.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 import './register.css';
 import { Button } from 'shards-react';
+import { authUrl } from '../../url';
+import LoginModal from './loginModalComponent';
+import JoinModal from './joinModalComponent';
 
 class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loginModalOpen: false,
+      joinModalOpen: false,
+      createModalOpen: false
+    }
+  }
+  
+  toggleJoinModal = () => {
+    this.setState({
+      joinModalOpen: !this.state.joinModalOpen
+    });
+  }
+
+  toggleLoginModal = () => {
+    this.setState({
+      loginModalOpen: !this.state.loginModalOpen
+    });
+  }
+
+  toggleCreateModal = () => {
+    this.setState({
+      createModalOpen: !this.state.createModalOpen
+    });
+  }
+
   render() {
     return(
       <div className = "container">
@@ -11,12 +41,22 @@ class Register extends Component {
           <div className = "col-6">
             <h4>Create your own team</h4>
             <br/>
-            <Button>Create</Button>
+            {
+              document.cookie.split('; ').find(row => row.startsWith('jwt=')) === undefined ?  
+              <Button onClick = {this.toggleLoginModal} >Create</Button>
+              :
+              <Button onClick = {this.toggleCreateModal} >Create</Button>
+            }
           </div>
           <div className = "col-6">
             <h4>Join an already existing team</h4>
             <br/>
-            <Button>Join</Button>
+            {
+              document.cookie.split('; ').find(row => row.startsWith('jwt=')) === undefined ?  
+              <Button onClick = {this.toggleLoginModal} >Join</Button>
+              :
+              <Button onClick = {this.toggleJoinModal} >Join</Button>
+            }
           </div>
         </div>
       </div>
