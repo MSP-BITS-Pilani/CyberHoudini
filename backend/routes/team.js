7461a7b52e8be88e80817585ccb6b6ed32d02f89
Kunjan-Shah <kunjan.nshah1@gmail.com>
2021-03-17 00:22:46 +0530

    team-api created

--- backend/routes/team.js
+++ backend/routes/team.js
@@ -2,25 +2,38 @@ const express = require("express");
 const teamRouter = express.Router();
 const Team = require("../models/team");
 const User = require("../models/user");
+const auth = require('../middleware/auth');
 const referralCode = require('../helper/referralCodeGenerator');
 
-teamRouter.get("/team", (req, res) => {
-    Team.find((err, teams) => {
-        if (err) {
-            console.error(err);
-        } else {
-            
-            if (teams.length == 0) {
-                var testTeams = [
-                    { teamName: 'AwesomeTeam', adminID: "60504e977332ae190828221b", lastCorrectAnswer : 'NULL', referralCode: referralCode(6) }
-                ];
-
-                // Team.collection.insertOne(testTeams, (err, teams) => { if (err) console.log(err); })
-            }
-            // res.send(requiredQuery);
-        }
-    });
+teamRouter.get("/", auth, async (req, res) => {
+    const user = req.user;
+    const teamID = user.teamID;
+    const team = await Team.findOne({_id : teamID});
+    if (!team) {
+        console.error('Team does not exist');
+    }
+    else {
+        res.send(team);
+    }
 });
 
+
+// const express = require("express");
+// const teamRouter = express.Router();
+// const Team = require("../models/team");
+// const auth = require('../middleware/auth');
+
+// teamRouter.get("/", auth, (req,res)=>{
+//     // if(!req.query.email){
+//     //     return res.send({
+//     //         error:"Please Enter email id"
+//     //     })
+//     // }
+//     // const email=req.query.email
+//     // //const email="xxx123@gmail.com"
+//     // const user=User.findOne({email: email})
+//     const user = req.user;
+    
+// })
 
 module.exports = teamRouter
