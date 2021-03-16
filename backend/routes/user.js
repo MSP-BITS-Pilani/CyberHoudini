453a58efdff28989462ef3be7dd18a5dea417828
Dwij Mehta <dwij.mehta@gmail.com>
2021-03-17 00:24:37 +0530

    GET /user returns team details

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,15 +1,26 @@
 const express = require("express")
 const userRouter = express.Router();
-const User = require("../models/user")
+const Team = require("../models/team")
 const auth = require("../middleware/auth")
 
-userRouter.get("/", auth, (req, res) => {
+userRouter.get("/", auth, async (req, res) => {
     const user = req.user
+
+
     if (!user) {
         console.log('Email address not registered')
     }
-    else
-        res.send(user)
+    else {
+        if (!user.teamID) {
+            const team = await Team.findById(user.teamID);
+            console.log(user.teamID);
+            res.send({ user, team })
+        }
+        else {
+            res.send({ user })
+        }
+    }
+
 })
 
 
