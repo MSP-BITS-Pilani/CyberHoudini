20c40c1b7fa58b70e014ddd305adebd42d36f3ec
madhavgupta211 <56752137+madhavgupta211@users.noreply.github.com>
2021-03-18 14:18:48 +0530

    IMAGE correction

--- backend/models/user.js
+++ backend/models/user.js
@@ -30,8 +30,8 @@ let userSchema = new Schema({
 
     picture: {
         type: String,
-        required: true,
-        lowercase: true
+        required: true
+        // lowercase: true
     },
 
     tokens: [{
