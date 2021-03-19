0276f2d8e610a3e3e0980225172e40e29ed31d46
Kunjan-Shah <56250322+Kunjan-Shah@users.noreply.github.com>
2021-03-19 14:29:05 +0530

    Question fetched from database and API changed (#39)
    * team.js created
    
    * removed package-lock.json
    
    * team-api created
    
    * Started working on team api (#12)
    
    * Added User details route (#9)
    
    * team.js created
    
    * removed package-lock.json
    
    * team-api created
    
    * POST /teams complete
    
    * join using rc added
    
    * created and resolved conflicts for team-api-register
    
    * Team API register created (#16)
    
    * team.js created
    
    * removed package-lock.json
    
    * team-api created
    
    * join using rc added
    
    * created and resolved conflicts for team-api-register
    
    * delete team feature created
    
    * Team api updated (#18)
    
    * team.js created
    
    * removed package-lock.json
    
    * team-api created
    
    * join using rc added
    
    * created and resolved conflicts for team-api-register
    
    * Team api delete (#19)
    
    * team.js created
    
    * removed package-lock.json
    
    * team-api created
    
    * join using rc added
    
    * created and resolved conflicts for team-api-register
    
    * delete team feature created
    
    * image issue resolved and team-api updated
    
    * Team API implemented (#23)
    
    * team.js created
    
    * removed package-lock.json
    
    * team-api created
    
    * join using rc added
    
    * created and resolved conflicts for team-api-register
    
    * delete team feature created
    
    * image issue resolved and team-api updated
    
    * Improved response status codes and responses of team-api(#20)
    
    * Added npm run dev to scripts
    
    * Improved response codes
    
    * Improved response code for delete
    
    * Added route for user to leave a tean (#27)
    
    * question api created for get request
    
    * question api created for get request
    
    * question-api for get and post created
    
    * lastCorrectAnswer added
    
    * question level fetched from database
    
    Co-authored-by: Dwij Mehta <dwij.mehta@gmail.com>
    Co-authored-by: Dwij Mehta <55494681+dwij2212@users.noreply.github.com>

--- backend/routes/question.js
+++ backend/routes/question.js
@@ -5,38 +5,43 @@ const answers = require("../answer");
 const Team = require("../models/team");
 const User = require("../models/user");
 const auth = require('../middleware/auth');
+const stageArray = ["-1","0","82","129","235","371","649","793","1139","1349","1679","2291","2573","?","14","15","16","99999999","-1"];
 
 questionRouter.get("/", auth, async(req, res) => {
-    const stage = req.body.questionID;
+    const user = req.user;
+    const teamID = user.teamID;
+    const team = await Team.findOne({ _id: teamID });
+    const level = parseInt(team.level);
+    const stage = stageArray[level + 1];
     const question = await Question.findOne({ stage: stage });
+    const questionIndex = {questionIndex : level + 1};
     if(!question) {
         console.error('No such stage');
         res.sendStatus(404);
     }
     else {
         console.log(question);
-        res.status(200).send(question);
+        res.status(200).send({question, questionIndex});
     }
 });
 
 questionRouter.post("/", auth, async(req, res) => {
-    const stage = req.body.questionID;
-    const qi = req.body.questionIndex;
-    const questionIndex = parseInt(qi);
-    const question = await Question.findOne({ stage: stage });
-    const points = question.points;
     const user = req.user;
     const teamID = user.teamID;
     const team = await Team.findOne({ _id: teamID });
+    const level = parseInt(team.level);
+    const stage = stageArray[level + 1];
+    const question = await Question.findOne({ stage: stage });
+    const points = question.points;
     const response = req.body.userAnswer;
     const status = {correct: false};
     const members = await User.find({ teamID: teamID });
 
-    if(response.toString() === answers.answers[questionIndex - 1]) {
+    if(response.toString() === answers.answers[level]) {
         status.correct = true;
         try {
             team.score = team.score + points;
-            team.level = questionIndex;
+            team.level = level + 1;
             team.lastCorrectAnswer = Date();
             await team.save();
             console.log("Your answer is correct, Score updated successfully");
