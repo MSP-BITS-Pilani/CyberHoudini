a1591d7c899b7e2e80a923e93f700ae3d429658a
madhavgupta211 <56752137+madhavgupta211@users.noreply.github.com>
2021-03-19 17:41:47 +0530

    stash

--- frontend/src/Containers/quiz/quizComponent.js
+++ frontend/src/Containers/quiz/quizComponent.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 import baseUrl from "../../baseUrl";
 import "./quiz.css";
 import './qFifteenBill/qFifteenBill';
-import QFifteenBill from './qFifteenBill/qFifteenBill';
+import { io } from 'socket.io-client';
+import { Link } from 'react-router-dom';
 
 import {
   Card, CardImg, CardText, CardBody,
@@ -11,16 +12,17 @@ import {
 } from 'reactstrap';
 
 class Quiz extends Component {
-  constructor(props){
-  super(props);
-  this.state = {
-      id: 0, 
+  constructor(props) {
+    super(props);
+    this.state = {
+      socket: io(),
+      id: 0,
       question: "Loading...",
       userAnswer: "",
       imgURL: null,
       hint: null,
       fadeIn: false,
-      encryptArray: ["-1","0","82","129","235","371","649","793","1139","1349","1679","2291","2573","?","14","15","16","99999999"]
+      encryptArray: ["-1", "0", "82", "129", "235", "371", "649", "793", "1139", "1349", "1679", "2291", "2573", "13", "14", "99999999"]
     }
   }
 
@@ -30,11 +32,11 @@ class Quiz extends Component {
 
   toggleHint = () => {
     this.setState((prevState, props) => {
-      return {fadeIn: !prevState.fadeIn}
+      return { fadeIn: !prevState.fadeIn }
     })
   }
 
-  getQuestion = async() => {
+  getQuestion = async () => {
     const cookies = document.cookie.split('; ');
     const value = cookies.find(item => item.startsWith('jwt')).split('=')[1];
     await axios({
@@ -43,7 +45,7 @@ class Quiz extends Component {
       headers: {
         Authorization: `Bearer ${value}`
       }
-    }).then( response => {
+    }).then(response => {
       console.log(response);
       this.setState({
         question: response.data.question.question,
@@ -51,9 +53,9 @@ class Quiz extends Component {
         hint: response.data.question.hint,
         id: response.data.questionIndex.questionIndex
       });
-    }).catch( error => {
+    }).catch(error => {
       console.log(error);
-    }) 
+    })
   }
 
   handleAnswerChange = (event) => {
@@ -77,15 +79,15 @@ class Quiz extends Component {
         userAnswer: this.state.userAnswer.toLowerCase().trim()
       }
     }).then(response => {
-       console.log(response);
-       if(response.data.status.correct === false){
-         alert("Wrong answer, try again");
-       }else{
+      console.log(response);
+      if (response.data.status.correct === false) {
+        alert("Wrong answer, try again");
+      } else {
         alert("Right answer.");
         this.setState((prevState, props) => {
           const updatedState = {
             userAnswer: "",
-            question: "Loading..."  ,
+            question: "Loading...",
             fadeIn: false
           };
           return updatedState;
@@ -95,67 +97,77 @@ class Quiz extends Component {
     })
   }
 
-  render(){
+  render() {
 
     let additionalClass = "";             // In this question color of background and question is same
-    if(this.state.id === 4){
-      additionalClass = "questionFour";      
+    if (this.state.id === 4) {
+      additionalClass = "questionFour";
     }
 
     let additionalDiv = null;
-    if(this.state.id === 8){         // for this question URL has to be embedded in page source
-        additionalDiv = <div style={{display: 'none'}}>https://mars.nasa.gov/mars2020/spacecraft/rover/</div>
-    }
-
-    if(this.state.id === 15){
-        additionalDiv = <QFifteenBill />
+    if (this.state.id === 8) {         // for this question URL has to be embedded in page source
+      additionalDiv = <div style={{ display: 'none' }}>https://mars.nasa.gov/mars2020/spacecraft/rover/</div>
     }
 
     let toDisplay = (
       <div>
 
-        <Container style={{margin: "2rem auto"}}> {/*Question Div */}
+        <Container style={{ margin: "2rem auto" }}> {/*Question Div */}
           <Card>
-          {this.state.imgURL !== null && 
-            <CardImg top width="100%" src={this.state.imgURL} alt="Card image cap" />}
-          {additionalDiv}
-        <CardBody>
-          <CardTitle tag="h5">Stage number - {this.state.encryptArray[this.state.id]}</CardTitle>
-          <CardText className={additionalClass}>{this.state.question}</CardText>
-          {this.state.hint && 
-            <React.Fragment>
-            <Button color="primary" outline size="sm" onClick={this.toggleHint}>Show Hint</Button>
-          
-            <Fade in={this.state.fadeIn} className="mt-3">
-              {this.state.hint}
-            </Fade>
-            
-          </React.Fragment>}
-          
-          
-          
-        </CardBody>
-      </Card>
+            {this.state.imgURL !== null &&
+              <CardImg top width="100%" src={this.state.imgURL} alt="Card image cap" />}
+            {additionalDiv}
+            <CardBody>
+              <CardTitle tag="h5">Stage number - {this.state.id}</CardTitle>
+              <CardText className={additionalClass}>{this.state.question}</CardText>
+              {this.state.hint &&
+                <React.Fragment>
+                  <Button color="primary" outline size="sm" onClick={this.toggleHint}>Show Hint</Button>
 
+                  <Fade in={this.state.fadeIn} className="mt-3">
+                    {this.state.hint}
+                  </Fade>
+
+                </React.Fragment>}
+
+
+
+            </CardBody>
+          </Card>
         </Container>
 
         <Container>
-        <Form onSubmit={this.handleAnswerSubmit}>
-      <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-        <Label className="mr-sm-2" tag="h6">Enter your answer - </Label>
-        <Input type="text" autoComplete = "off" name="answer" value={this.state.userAnswer} onChange={this.handleAnswerChange} />
-      </FormGroup>
-      <Button color="primary" outline size="sm" type="submit">Submit</Button>
-    </Form>
+          <Form onSubmit={this.handleAnswerSubmit}>
+            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+              <Label className="mr-sm-2" tag="h6">Enter your answer - </Label>
+              <Input type="text" autoComplete="off" name="answer" value={this.state.userAnswer} onChange={this.handleAnswerChange} />
+            </FormGroup>
+            <Button color="primary" outline size="sm" type="submit">Submit</Button>
+          </Form>
         </Container>
 
-        
+
       </div>
 
     );
+    //render winning screen
+    if (this.state.id === 15) {           /*For a total of 16 stages + mario image*/
+      toDisplay =
+        <div className="container">
+          <div className="row">
+            <div className="col-12 text-center">
+              {/* <img className = "win-image" src="https://www.coachingforgeeks.com/wp-content/uploads/2018/06/34074321_10161183145705377_7560210874204422144_n.jpg" alt="Princess was in another castle" /> */}
+              <h2>Congratulations. You are so lonely that you might just deserve a prize for that. See the leaderboard if you won or not.</h2>
+              <Link to="/">
+                <Button onClick="home-register">Redirect to home</Button>&nbsp;
+            </Link>
+              <Link to="/leaderboard">
+                <Button onClick="home-register">See leaderboard</Button>
+              </Link>
 
-    if(this.state.id === 17){           /*For a total of 16 stages + mario image*/
-       toDisplay = <img src="https://www.coachingforgeeks.com/wp-content/uploads/2018/06/34074321_10161183145705377_7560210874204422144_n.jpg" alt="Princess was in another castle" />
+            </div>
+          </div>
+        </div>
     }
 
     return (
